6859c50a538e5d5b0e8adc23871caef0e1fcd1a4
Pourya Moghadam <p.moghadam@msn.com>
2022-08-12 21:33:16 +0430

    - bug fix require packages

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="wallex-python",
-    version="0.0.2",
+    version="0.0.3",
     author="Pourya Moghadam",
     author_email="p.moghadam@msn.com",
     description="Wallex Exchange Unofficial API Python Client",
@@ -15,7 +15,7 @@ setuptools.setup(
     url="https://github.com/wallexchange/wallex-python",
     license="MIT",
     keywords='wallex exchange api bitcoin ethereum btc eth python',
-    install_requires=['pydantic', 'requests'],
+    install_requires=['pydantic', 'requests', 'certifi', 'charset-normalizer', 'idna', 'typing_extensions', 'urllib3'],
     project_urls={
         "Bug Tracker": "https://github.com/wallexchange/wallex-python/issues",
         "Documentation": "https://api-docs.wallex.ir/",
